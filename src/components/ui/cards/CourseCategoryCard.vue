170ebab66b880c0c48cb39d76dbf28d47d5f80f7
WebCONuz <muxammadi0799@gmail.com>
2023-07-25 11:18:35 +0500

    Changed CourseCategory card

--- src/components/ui/cards/CourseCategoryCard.vue
+++ src/components/ui/cards/CourseCategoryCard.vue
@@ -8,10 +8,10 @@ const props = defineProps({
 </script>
 
 <template>
-    <WrapBox class="flex items-center">
+    <WrapBox class="course flex items-center mb-4 cursor-default duration-200">
         <div 
             class="w-[50px] h-[50px] flex items-center justify-center text-white text-2xl rounded-md" 
-            :class="'bg-[' + props.textColor + ']'" 
+            :style="'background-color:' + props.textColor" 
         >W</div>
         <div class="ml-4">
             <h3 class="text-[#191d33]">Web Developing</h3>
@@ -19,11 +19,16 @@ const props = defineProps({
         </div>
         <LinkBtn 
             class="ml-auto" 
-            :class="'bg-[' + props.bgColor + ']' + ' ' + 'text-[' + props.textColor + ']'" 
+            :style="'background-color:' + props.bgColor + '; ' + 'color:' + props.textColor + ';'" 
             url="/admin/category/2"
             text="View Courses"
         />
     </WrapBox>
 </template>
 
-<style scoped></style> 
+<style scoped>
+.course:hover{
+    transform: scale(1.02) translateY(-4px);
+    box-shadow: 0 5px 15px 0 rgba(0, 0, 0, 0.2);
+}
+</style> 
--- src/views/adminViews/DashboardView.vue
+++ src/views/adminViews/DashboardView.vue
@@ -6,9 +6,21 @@ import CourseCategoryCard from '../../components/ui/cards/CourseCategoryCard.vue
 <template>
   <div>
     <Breadcrumb title="Dashboard" :data="['Dashboard', 'Main']" />
+    
     <div class="flex justify-between">
       <div class="box-container">
         <CourseCategoryCard bgColor="#FEF5D4" textColor="#fec801" />
+        <CourseCategoryCard bgColor="#FCECF1" textColor="#F2426D" />
+        <CourseCategoryCard bgColor="#D7F3EF" textColor="#58BAAB" />
+        <CourseCategoryCard bgColor="#E2F3FC" textColor="#019FF8" />
+      </div>
+      <div class="box-container">
+        <CourseCategoryCard bgColor="#D7F3EF" textColor="#58BAAB" />
+        <CourseCategoryCard bgColor="#FEF5D4" textColor="#fec801" />
+      </div>
+      <div class="box-container">
+        <CourseCategoryCard bgColor="#FEF5D4" textColor="#fec801" />
+        <CourseCategoryCard bgColor="#D7F3EF" textColor="#58BAAB" />
       </div>
     </div>
   </div>
@@ -16,6 +28,6 @@ import CourseCategoryCard from '../../components/ui/cards/CourseCategoryCard.vue
 
 <style scoped>
 .box-container{
-  width: calc(100% / 3 - 5px);
+  width: calc(100% / 3 - 10px);
 }
 </style>
